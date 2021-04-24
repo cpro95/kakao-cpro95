5909ac84510eb0a7eca1ddee753c0ad322efbb25
cpro95 <cpro95@users.noreply.github.com>
2021-04-24 09:32:49 +0900

    update footer.js for tutorial link

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -22,7 +22,7 @@ const Footer = (props) => {
     <div>
       <Paper className={classes.root} elevation={0}>
         <Typography component="p">
-          <a href="https://blog.naver.com/cpro95" className={classes.link}>
+          <a href="https://cpro95.tistory.com/50" className={classes.link}>
             링크 : 튜토리얼 보기
           </a>
           <p>카카오톡 API와 React, Material-UI로 만들었습니다.</p>
