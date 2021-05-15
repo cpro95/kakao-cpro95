7b6aefa19be23e5ebbe3347b92f86e91778d60e5
Manki KIm <cpro95@gmail.com>
2021-05-15 17:54:35 +0900

    google adsense module removed

--- src/components/Header.js
+++ src/components/Header.js
@@ -3,7 +3,6 @@ import AppBar from "@material-ui/core/AppBar";
 import Toolbar from "@material-ui/core/Toolbar";
 import Typography from "@material-ui/core/Typography";
 import { makeStyles } from "@material-ui/core/styles";
-import AdSense from "react-adsense";
 
 const useStyles = makeStyles((theme) => ({
   margin: {
@@ -19,14 +18,6 @@ const Header = (props) => {
 
   return (
     <div className={classes.margin}>
-      <AdSense.Google
-        client="ca-pub-7748316956330968"
-        slot="7806394673"
-        style={{ display: "block" }}
-        format="auto"
-        responsive="true"
-        layoutKey="-gw-1+2a-9x+5c"
-      />
       <AppBar position="static">
         <Toolbar>
           <Typography variant="h5" color="inherit" className={classes.flex}>
