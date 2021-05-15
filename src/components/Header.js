3631f96c68371216bd51717bc6f2aa3ee2b84567
Manki KIm <cpro95@gmail.com>
2021-05-15 17:51:43 +0900

    google adsens in react module added

--- src/components/Header.js
+++ src/components/Header.js
@@ -3,6 +3,7 @@ import AppBar from "@material-ui/core/AppBar";
 import Toolbar from "@material-ui/core/Toolbar";
 import Typography from "@material-ui/core/Typography";
 import { makeStyles } from "@material-ui/core/styles";
+import AdSense from "react-adsense";
 
 const useStyles = makeStyles((theme) => ({
   margin: {
@@ -18,6 +19,14 @@ const Header = (props) => {
 
   return (
     <div className={classes.margin}>
+      <AdSense.Google
+        client="ca-pub-7748316956330968"
+        slot="7806394673"
+        style={{ display: "block" }}
+        format="auto"
+        responsive="true"
+        layoutKey="-gw-1+2a-9x+5c"
+      />
       <AppBar position="static">
         <Toolbar>
           <Typography variant="h5" color="inherit" className={classes.flex}>
