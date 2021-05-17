dab080ee706e6aa93479880202f64177dd9d2513
Manki KIm <cpro95@gmail.com>
2021-05-17 18:36:44 +0900

    tutorial link now open in a new tab

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -22,7 +22,11 @@ const Footer = (props) => {
     <div>
       <Paper className={classes.root} elevation={0}>
         <Typography component="p">
-          <a href="https://cpro95.tistory.com/50" className={classes.link}>
+          <a
+            href="https://cpro95.tistory.com/50"
+            target="_blank"
+            className={classes.link}
+          >
             링크 : 튜토리얼 보기
           </a>
           <p>카카오톡 API와 React, Material-UI로 만들었습니다.</p>
